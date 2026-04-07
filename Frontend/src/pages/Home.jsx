4b7d1306d4de6754073402dec51f4d8fc24d175f
kwesimain <opokulive@gmail.com>
2026-04-07 02:13:23 +0000

    fix background image

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -80,7 +80,7 @@ const Home = () => {
       {/* Background */}
       <div 
         className="fixed inset-0 bg-cover bg-center bg-no-repeat z-0"
-        style={{ backgroundImage: "url('/src/assets/HomeBackground/IMG_9453.jpg')" }}
+        style={{ backgroundImage: "url('src/assets/HomeBackground/IMG_9453.jpg')" }}
       >
         <div className="absolute inset-0 bg-black/70"></div>
       </div>
